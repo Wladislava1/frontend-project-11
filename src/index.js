29f81b07f8174265fda5bad8218bc42be3aaae6f
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-02-27 20:15:51 +0300

    bug - updating the page when submitting the form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './styles.scss';
 import 'bootstrap';
+import * as yup from 'yup';
+
+export default () => {
+  const feeds = [];
+  console.log(feeds);
+  const linkSchema = yup.object().shape({
+    url: yup.string().url().required().notOneOf(feeds),
+  });
+
+  const validateForm = (data) => linkSchema.validate(data)
+    .then(() => {
+      feeds.push(data);
+      console.log('ok');
+    })
+    .catch((error) => {
+      console.log('error', error.message);
+    });
+
+  const formInput = document.querySelector('.rss-form');
+  formInput.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const urlInput = document.querySelector('input[id="url-input"]');
+    const url = urlInput.value.trim();
+    validateForm({ url });
+  });
+};
