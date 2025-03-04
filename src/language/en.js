1a859549a553773b530a1cdf473a077f06d43327
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-03-04 22:11:40 +0300

    add feeds

--- src/language/en.js
+++ src/language/en.js
@@ -3,5 +3,6 @@ export default {
     code_zero: 'RSS uploaded successfully',
     code_one: 'The link must be a valid URL',
     code_two: 'RSS already exists',
+    feeds: 'Feeds',
   },
 };
--- src/language/ru.js
+++ src/language/ru.js
@@ -3,5 +3,6 @@ export default {
     code_zero: 'RSS успешно загружен',
     code_one: 'Ссылка должна быть валидным URL',
     code_two: 'RSS уже существует',
+    feeds: 'Фиды',
   },
 };
