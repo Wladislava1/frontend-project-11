47ac391db2ffa183e8ce6b52a30eec51f14256e4
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-03-06 22:22:21 +0300

    add event for posts and update function for get posts and feeds

--- src/rssForm.js
+++ src/rssForm.js
@@ -9,6 +9,7 @@ export default function rssForm() {
     feeds: [], // id url title description
     posts: [],
     error: null,
+    viewPost: null,
   };
 
   const watchedState = initView(state);
@@ -20,9 +21,9 @@ export default function rssForm() {
       .test('unique-url', 2, (value) => !state.feeds.some((item) => item.url === value)),
   });
 
-  const functionResponse = (errorCode, validUrl) => {
+  const getFeedsAndPosts = (errorCode, url) => {
     if (errorCode === 0) {
-      fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(validUrl)}`)
+      fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
         .then((response) => {
           if (response.ok) return response.json();
           throw new Error('Network response was not ok.');
@@ -30,26 +31,44 @@ export default function rssForm() {
         .then((data) => {
           const parser = new DOMParser();
           const doc = parser.parseFromString(data.contents, 'application/xml');
-          const title = doc.querySelector('channel > title')?.textContent || 'Без названия';
-          const description = doc.querySelector('channel > description')?.textContent || 'Без описания';
-          const index = state.feeds.findIndex((item) => item.url === validUrl);
-          if (index !== -1) {
-            state.feeds[index].title = title;
-            state.feeds[index].description = description;
-            const updateFeeds = [...state.feeds];
-            watchedState.feeds = updateFeeds;
+          const titleFeed = doc.querySelector('channel > title')?.textContent || 'Без названия';
+          const descriptionFeed = doc.querySelector('channel > description')?.textContent || 'Без описания';
+          const indexFeed = state.feeds.findIndex((item) => item.url === url);
+
+          if (indexFeed !== -1) {
+            state.feeds[indexFeed].title = titleFeed;
+            state.feeds[indexFeed].description = descriptionFeed;
           }
+          const postsItem = doc.querySelectorAll('item');
+          const feedId = state.feeds[indexFeed].id;
+          const newPosts = Array.from(postsItem).map((item) => {
+            const titlePost = item.querySelector('title')?.textContent;
+            const linkPost = item.querySelector('link')?.textContent;
+            return {
+              id: uniqid(),
+              feedId,
+              title: titlePost,
+              url: linkPost,
+            };
+          });
+          state.posts = [...newPosts, ...state.posts];
+          watchedState.feeds = [...state.feeds];
+          watchedState.posts = [...state.posts];
+          console.log(state.posts);
+          console.log(state.feeds);
         });
     }
   };
 
   const validateForm = (data) => linkSchema.validate(data)
     .then(() => {
-      state.feeds.push({ id: uniqid(), url: data.url });
+      const idFeedPost = uniqid();
+      state.feeds.push({ id: idFeedPost, url: data.url });
+      state.posts.unshift({ id: uniqid(), feedId: idFeedPost });
       watchedState.error = 0;
       console.log(state.feeds);
       console.log(watchedState.error);
-      functionResponse(watchedState.error, data.url);
+      getFeedsAndPosts(watchedState.error, data.url);
     })
     .catch((error) => {
       watchedState.error = error.message;
@@ -62,5 +81,17 @@ export default function rssForm() {
     const urlInput = document.querySelector('input[id="url-input"]');
     const url = urlInput.value.trim();
     validateForm({ url });
+  });
+
+  const containerPosts = document.querySelector('.posts');
+  containerPosts.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.tagName === 'BUTTON') {
+      const link = e.target.previousElementSibling;
+      if (link && link.tagName === 'A') {
+        const title = link.textContent;
+        watchedState.viewPost = title;
+      }
+    }
   });
 }
