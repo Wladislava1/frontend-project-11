9b3244e537d4bf9482f3f19f661b55ea5f2dcc18
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-03-12 21:21:32 +0300

    add posts

--- src/language/en.js
+++ src/language/en.js
@@ -3,6 +3,7 @@ export default {
     code_zero: 'RSS uploaded successfully',
     code_one: 'The link must be a valid URL',
     code_two: 'RSS already exists',
+    code_tree: 'The resource does not contain valid RSS',
     feeds: 'Feeds',
     posts: 'Posts',
   },
--- src/language/ru.js
+++ src/language/ru.js
@@ -3,6 +3,7 @@ export default {
     code_zero: 'RSS успешно загружен',
     code_one: 'Ссылка должна быть валидным URL',
     code_two: 'RSS уже существует',
+    code_tree: 'Ресурс не содержит валидный RSS',
     feeds: 'Фиды',
     posts: 'Посты',
   },
