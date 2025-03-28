58ed29d83ec594a8c5b70ed2506470ce975ac5b8
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-03-28 21:29:05 +0300

    fix errors in tests tests_

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,16 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('has title', ({ page }) => {
+  page.goto('https://playwright.dev/').then(() => {
+    expect(page).toHaveTitle(/Playwright/);
+  });
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('get started link', ({ page }) => {
+  page.goto('https://playwright.dev/').then(() => {
+    page.getByRole('link', { name: 'Get started' }).click().then(() => {
+      expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    });
+  });
 });
