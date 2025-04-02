05abf8a914dd8dee93e7ae4d56f95f1bc70adec7
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-04-02 21:42:52 +0300

    remove Promise

--- src/view/i18n.js
+++ src/view/i18n.js
@@ -2,22 +2,15 @@ import i18next from 'i18next';
 import ru from '../language/ru.js';
 import en from '../language/en.js';
 
-const initI18n = () => new Promise((resolve, reject) => {
-  const i18nInstance = i18next.createInstance();
-  i18nInstance.init({
-    lng: 'ru',
-    debug: true,
-    resources: {
-      ru: { translation: ru.translation },
-      en: { translation: en.translation },
-    },
-  })
-    .then(() => {
-      resolve(i18nInstance);
-    })
-    .catch((error) => {
-      reject(error);
-    });
+const initI18n = () => i18next.createInstance().init({
+  lng: 'ru',
+  debug: true,
+  resources: {
+    ru: { translation: ru.translation },
+    en: { translation: en.translation },
+  },
+}).catch((error) => {
+  console.error('Ошибка инициализации i18next:', error);
 });
 
 export default initI18n;
