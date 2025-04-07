8d52f6695a2f4ff044cccb34b1bbcb3c98e54142
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-04-07 15:21:17 +0300

    pulled functions from rssForm() replaced test in schema with notOneOf() changed state assignment in update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import './styles.scss';
 import 'bootstrap';
 import initView from './view/index.js';
@@ -7,105 +8,119 @@ import parseRssFeed from './parser.js';
 import fetchRssFeed from './fetch.js';
 import intervalUpdateFeeds from './intervalUpdatesFeeds.js';
 
-export default function rssForm() {
-  const state = initialState;
-  const watchedState = initView(state);
-  const linkSchema = getSchema(watchedState);
-
-  const getFeedsAndPosts = (url) => {
-    watchedState.addingFeedProcess = {
-      ...watchedState.addingFeedProcess,
-      state: 'processing',
-    };
-    return fetchRssFeed(url)
-      .then((data) => {
-        const { titleFeed, descriptionFeed, posts } = parseRssFeed(data.contents);
-        const existingFeed = state.feeds.find((feed) => feed.url === url);
-        console.log(`existingFeed: ${existingFeed}`);
-
-        if (existingFeed) {
-          existingFeed.title = titleFeed;
-          existingFeed.description = descriptionFeed;
-        } else {
-          const newFeed = createFeed(url, titleFeed, descriptionFeed);
-          watchedState.feeds = [...state.feeds, newFeed];
-          watchedState.posts = [createPost(newFeed.id), ...state.posts];
-        }
-        console.log(`state.feeds: ${state.feeds}`);
-        const feedId = state.feeds.find((feed) => feed.url === url).id;
-        const newPosts = posts.map((post) => createPost(
-          feedId,
-          post.title,
-          post.description,
-          post.url,
-        ));
-        watchedState.posts = [...newPosts, ...state.posts];
-        watchedState.addingFeedProcess = { ...watchedState.addingFeedProcess, state: 'success' };
-        intervalUpdateFeeds(state, watchedState);
-      })
-      .catch((error) => {
-        watchedState.addingFeedProcess = { ...watchedState.addingFeedProcess, state: 'failed', error: error.message };
-      });
+const getFeedsAndPosts = (url, watchedState, state) => {
+  watchedState.addingFeedProcess = {
+    ...watchedState.addingFeedProcess,
+    state: 'processing',
   };
-  const validateForm = (data) => linkSchema.validate(data)
+  return fetchRssFeed(url)
+    .then((data) => {
+      const { titleFeed, descriptionFeed, posts } = parseRssFeed(data.contents);
+      const existingFeed = state.feeds.find((feed) => feed.url === url);
+      console.log(`existingFeed: ${existingFeed}`);
+
+      if (existingFeed) {
+        existingFeed.title = titleFeed;
+        existingFeed.description = descriptionFeed;
+      } else {
+        const newFeed = createFeed(url, titleFeed, descriptionFeed);
+        watchedState.feeds = [...state.feeds, newFeed];
+        watchedState.posts = [createPost(newFeed.id), ...state.posts];
+      }
+      console.log(`state.feeds: ${state.feeds}`);
+      const feedId = state.feeds.find((feed) => feed.url === url).id;
+      const newPosts = posts.map((post) => createPost(
+        feedId,
+        post.title,
+        post.description,
+        post.url,
+      ));
+      watchedState.posts = [...newPosts, ...state.posts];
+      watchedState.addingFeedProcess = { ...watchedState.addingFeedProcess, state: 'success' };
+      intervalUpdateFeeds(state, watchedState);
+    })
+    .catch((error) => {
+      watchedState.addingFeedProcess = { ...watchedState.addingFeedProcess, state: 'failed', error: error.message };
+    });
+};
+
+const validateForm = (data, watchedState) => {
+  const linkSchema = getSchema(watchedState);
+  return linkSchema.validate(data)
     .then(() => true)
     .catch((error) => {
       watchedState.addingFeedProcess = { ...watchedState.addingFeedProcess, state: 'failed', error: error.message };
       return false;
     });
+};
+
+const handleFormSubmit = (e, urlInput, watchedState, state) => {
+  e.preventDefault();
+  const url = urlInput.value.trim();
+  validateForm({ url }, watchedState)
+    .then((isValid) => {
+      if (!isValid) return;
+      watchedState.addingFeedProcess = { ...watchedState.addingFeedProcess, state: 'processing' };
+      getFeedsAndPosts(url, watchedState, state);
+    });
+};
+
+const handlePostClick = (e, state, watchedState) => {
+  const post = e.target.closest('li');
+  if (!post) return;
+
+  const link = post.querySelector('a');
+  if (!link) return;
+
+  const postUrl = link.getAttribute('href');
+  const foundPost = state.posts.find((item) => item.url === postUrl);
+  if (foundPost) {
+    const isViewed = state.uiState.viewedPosts.some((viewedPost) => viewedPost.url === postUrl);
+    if (!isViewed) {
+      watchedState.uiState.viewedPosts = [...watchedState.uiState.viewedPosts, {
+        url: foundPost.url,
+        title: foundPost.title,
+        description: foundPost.description,
+        visibility: 'hidden',
+      }];
+    }
+  }
+  if (e.target.classList.contains('btn-sm')) {
+    watchedState.uiState.modalWindow = 'shown';
+    watchedState.uiState = { ...watchedState.uiState };
+  }
+};
+
+const handleCloseModal = (watchedState) => {
+  watchedState.uiState.modalWindow = 'hidden';
+  watchedState.uiState = { ...watchedState.uiState };
+};
+
+export default function rssForm() {
+  const state = initialState;
+  const watchedState = initView(state);
 
   const formInput = document.querySelector('.rss-form');
-  formInput.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const urlInput = document.querySelector('input[id="url-input"]');
-    const url = urlInput.value.trim();
-    validateForm({ url })
-      .then((isValid) => {
-        if (!isValid) return;
-        watchedState.addingFeedProcess = { ...watchedState.addingFeedProcess, state: 'processing' };
-        getFeedsAndPosts(url);
-      });
-  });
-
+  const urlInput = document.querySelector('input[id="url-input"]');
   const containerPosts = document.querySelector('.posts');
-  containerPosts.addEventListener('click', (e) => {
-    const post = e.target.closest('li');
-    if (!post) return;
-
-    const link = post.querySelector('a');
-    if (!link) return;
-
-    const postUrl = link.getAttribute('href');
-    const foundPost = state.posts.find((item) => item.url === postUrl);
-    console.log(`foundPost = ${foundPost}`);
-    if (foundPost) {
-      const isViewed = state.uiState.viewedPosts.some((viewedPost) => viewedPost.url === postUrl);
-      if (!isViewed) {
-        watchedState.uiState.viewedPosts = [...watchedState.uiState.viewedPosts, {
-          url: foundPost.url,
-          title: foundPost.title,
-          description: foundPost.description,
-          visibility: 'hidden',
-        }];
-      }
-    }
-    if (e.target.classList.contains('btn-sm')) {
-      watchedState.uiState.modalWindow = 'shown';
-      watchedState.uiState = { ...watchedState.uiState };
-    }
-  });
-
   const containerShow = document.querySelector('.fade');
   const closeModalButtonText = containerShow.querySelector('.close');
   const closeModalButtonKrest = containerShow.querySelector('.btn-secondary');
+
+  formInput.addEventListener('submit', (e) => handleFormSubmit(
+    e,
+    urlInput,
+    watchedState,
+    state,
+  ));
+
+  containerPosts.addEventListener('click', (e) => handlePostClick(e, state, watchedState));
   closeModalButtonText.addEventListener('click', (e) => {
     e.preventDefault();
-    watchedState.uiState.modalWindow = 'hidden';
-    watchedState.uiState = { ...watchedState.uiState };
+    handleCloseModal(watchedState);
   });
   closeModalButtonKrest.addEventListener('click', (e) => {
     e.preventDefault();
-    watchedState.uiState.modalWindow = 'hidden';
-    watchedState.uiState = { ...watchedState.uiState };
+    handleCloseModal(watchedState);
   });
 }
--- src/shema.js
+++ src/shema.js
@@ -1,10 +1,13 @@
 import * as Yup from 'yup';
 
-const getSchema = (state) => Yup.object().shape({
-  url: Yup.string()
-    .url(1)
-    .required()
-    .test('unique-url', 2, (value) => !state.feeds.some((item) => item.url === value)),
-});
+const getSchema = (state) => {
+  const urlsFeed = state.feeds.map((feed) => feed.url);
 
+  return Yup.object().shape({
+    url: Yup.string()
+      .url(1)
+      .required()
+      .notOneOf(urlsFeed, 2),
+  });
+};
 export default getSchema;
